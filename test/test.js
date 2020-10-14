a4fce16b5512176343bd745d189347f09328052e
nayan-patni <nayan@linkites.com>
2020-10-14 10:49:48 +0530

    simple node app with test

---
+++ test/test.js
@@ -0,0 +1,11 @@
+var request = require('supertest')
+var app = require('../server');
+
+
+describe('GET /', function (){
+it('respond with hello world', function(done){
+ 
+    // navigate to root and check the the response is "hello world"
+request(app).get('/').expect('hello world', done);
+});
+});
